8772e2f4e3ce24af4e36841775e5c74078c75356
HazemHassine <hazemhassine.edu@gmail.com>
2026-08-27 02:04:22 +0200

    chore: replace middleware with proxy

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { jwtVerify } from 'jose';
 
-export async function middleware(request) {
+export async function proxy(request) {
   const path = request.nextUrl.pathname;
 
   // Paths that require authentication
